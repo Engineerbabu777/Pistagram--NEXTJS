8b007c2fb0b5ac08ad4f84353c678cb994e015dd
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-02-16 17:55:47 +0500

    Added

--- components/FollowingModal.tsx
+++ components/FollowingModal.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Follower from "@/components/Follower";
 import {
   Dialog,
   DialogContent,
@@ -8,43 +7,43 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import useMount from "@/hooks/useMount";
-import { FollowerWithExtras } from "@/lib/definations";
+import { FollowingWithExtras } from "@/lib/definations";
 import { usePathname, useRouter } from "next/navigation";
+import Following from "./Following";
+import UserAvatar from "./UserAvatar";
 import { ScrollArea } from "./ui/scroll-area";
 
-function FollowersModal({
-  followers,
+function FollowingModal({
+  following,
   username,
 }: {
-  followers: FollowerWithExtras[] | undefined;
+  following: FollowingWithExtras[] | undefined;
   username: string;
 }) {
   const mount = useMount();
   const pathname = usePathname();
   const router = useRouter();
-  const isFollowersPage = pathname === `/dashboard/${username}/followers`;
+  const isFollowingPage = pathname === `/dashboard/${username}/following`;
 
   if (!mount) return null;
 
   return (
     <Dialog
-      open={isFollowersPage}
+      open={isFollowingPage}
       onOpenChange={(isOpen) => !isOpen && router.back()}
     >
       <DialogContent className="dialogContent">
         <DialogHeader className="border-b border-zinc-300 dark:border-neutral-700 py-2 w-full">
           <DialogTitle className="mx-auto font-bold text-base">
-            Followers
+            Following
           </DialogTitle>
         </DialogHeader>
 
-        {followers?.length === 0 && (
-          <p className="p-4 text-sm font-medium">This user has no followers.</p>
-        )}
+        {following?.length === 0 && <p>This user has no following.</p>}
 
         <ScrollArea className="min-h-fit max-h-[350px]">
-          {followers?.map((follower) => (
-            <Follower key={follower.followerId} follower={follower} />
+          {following?.map((following) => (
+            <Following key={following.followingId} following={following} />
           ))}
         </ScrollArea>
       </DialogContent>
@@ -52,4 +51,4 @@ function FollowersModal({
   );
 }
 
-export default FollowersModal;
+export default FollowingModal;
